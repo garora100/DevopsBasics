08b3ab573091339458f7ae186b2648f2afb052d4
Gaurav Arora <g.arora100@gmail.com>
2024-08-07 19:46:22 +0530

    added committed line in Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,6 +13,7 @@ public class Greeter {
   }
 
   //TODO: Add javadoc comment
+  //Some line are added to check the repository working
   public String greet(String someone) {
     return String.format("Hello How are you, %s!", someone);
   }
